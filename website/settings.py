4bb0cdd2b7917736e6f42d8b1de4b798c0b6bfa2
Elek <Elek2@yandex.ru>
2023-03-22 14:34:51 +0300

    Change password

--- website/settings.py
+++ website/settings.py
@@ -78,7 +78,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
         'USER': 'postgres',
-        'PASSWORD': 'nicaragua21',
+        'PASSWORD': '',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
